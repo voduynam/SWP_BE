1482883ccd7f290e571ca6b8080bab79e0c07221
voduynam <voduy1176@voduy1176gmail.com>
2026-03-14 20:06:59 +0700

    fixx crash

--- src/middlewares/uploadDeliveryImage.js
+++ src/middlewares/uploadDeliveryImage.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const CloudinaryStorage = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
